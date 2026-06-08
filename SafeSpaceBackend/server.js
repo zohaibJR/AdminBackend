df8882199b105f1a166f88de31658625d8f7dc6d
Zohaib UR Rehman <100087461+zohaibJR@users.noreply.github.com>
2026-06-09 00:25:00 +0500

    created backend project
    created backend project

---
+++ SafeSpaceBackend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
